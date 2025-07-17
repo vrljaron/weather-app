c1bbf5ed3559c53412b11d40431dda78b016afc5
Váraljai Áron <h986133@stud.u-szeged.hu>
2025-07-17 13:17:51 +0200

    Created a forecast API call, and cache data for the city

--- backend/index.js
+++ backend/index.js
@@ -2,29 +2,52 @@ import express from "express";
 import dotenv from "dotenv";
 import fetch from "node-fetch";
 import cors from "cors";
+import { getCachedCity, setCachedCity } from "./weatherCache";
 
 dotenv.config();
 const app = express();
-const PORT = 3001;
-
 app.use(cors());
 
-app.get("/weather", async (req, res) => {
+const PORT = 3001;
+const apiKey = process.env.OPEN_WEATHER_API_KEY;
+const currentWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric&lang=hu`;
+const forecastWeatherUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric&lang=hu`;
+
+app.get("/weather/full", async (req, res) => {
   const city = req.query.city;
   if (!city) return res.status(400).json({ error: "Missing city param" });
 
+  const cachedCity = getCachedCity(city);
+  if (cachedCity) {
+    console.log(`Fresh cached data for: ${city}`);
+    return res.json({ ...cachedCity.data, cached: true });
+  }
+
   console.log(`New fetch call for: ${city}`);
-  const apiKey = process.env.OPEN_WEATHER_API_KEY;
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric&lang=hu`;
 
   try {
-    const respone = await fetch(url);
-    if (!respone.ok) throw new Error("Unable to fetch city Data");
-    const weatherData = await respone.json();
-    res.json(weatherData);
-    console.log("successful fetch call");
+    const [currentWeatherRes, forecastWeatherRes] = await Promise.all([
+      fetch(currentWeatherUrl),
+      fetch(forecastWeatherUrl),
+    ]);
+
+    if (!currentWeatherRes.ok || !forecastWeatherRes.ok) {
+      throw new Error("Unable to fetch city Data from the API");
+    }
+
+    const currentWeatherData = await currentWeatherRes.json();
+    const forecastWeatherData = await forecastWeatherRes.json();
+
+    const responseData = {
+      city: city,
+      weatherData: currentWeatherData,
+      forecastWeatherData,
+    };
+
+    setCachedCity(city, responseData);
+
+    res.json({ responseData, cached: false });
   } catch (err) {
-    console.log(err.message);
     res.status(500).json({ error: err.message });
   }
 });
